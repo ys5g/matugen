4b0c22b4d7da7df6573520cad2a35b164611ddec
YS5G <yasir.gva@gmail.com>
2024-07-08 14:06:10 -0400

    fix(nix package): Fixed usage of lib.fileset.unions
    I should have re-enabled release-plz by the time I try to PR this fork

--- nix/default.nix
+++ nix/default.nix
@@ -1,14 +1,18 @@
 {pkgs, lib}: let
   manifest = (pkgs.lib.importTOML ../Cargo.toml).package;
+  fileset = lib.fileset.unions [
+    ../src
+    ../Cargo.lock
+    ../Cargo.toml
+  ];
 in
   pkgs.rustPlatform.buildRustPackage {
     pname = manifest.name;
     version = manifest.version;
     cargoLock.lockFile = ../Cargo.lock;
-    src = lib.fileset.unions [
-      ../src
-      ../Cargo.lock
-      ../Cargo.toml
-    ];
+    src = lib.fileset.toSource {
+      inherit fileset;
+      root = ../.;
+    };
     meta.mainProgram = "matugen";
   }
